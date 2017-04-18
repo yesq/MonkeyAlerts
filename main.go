8cdbfd8bd60f6092fc752f45f4cf23ee36eb3bcf
yesq <qiaopeng01@gmail.com>
2017-04-18 20:01:43 +0800

    Make Mail Client Daemon

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"time"
 
 	gomail "gopkg.in/gomail.v2"
 )
@@ -18,14 +19,17 @@ type Config struct {
 	SMTPPort int    `json:"smtpPort"`
 }
 
+// CH : channel to transport mail
+var CH chan *gomail.Message
+
 var config Config
+
 var d *gomail.Dialer
 
 func init() {
 	loadConfig()
 	fmt.Println(config)
-	d = gomail.NewDialer(config.SMTPURL, config.SMTPPort, config.Mail, config.Password)
-	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	go daemonMailClient()
 }
 
 func loadConfig() error {
@@ -41,14 +45,63 @@ func loadConfig() error {
 	return fmt.Errorf("open ./config.json err : %v", err)
 }
 
-func sendAlertSample(addTo string, body string) error {
+func sendAlertSample(addTo, body, title string) {
+	println("send line 1")
 	m := gomail.NewMessage()
 	m.SetHeader("From", config.Mail)
 	m.SetHeader("To", addTo)
+	m.SetHeader("Subject", title)
 	m.SetBody("text/html", body)
-	return d.DialAndSend(m)
+	// CH <- m
+	println("send CH")
+	CH <- m
+}
+
+func daemonMailClient() {
+	println("daemon start")
+	CH = make(chan *gomail.Message)
+	defer close(CH)
+	defer println("exec defer")
+	d = gomail.NewDialer(config.SMTPURL, config.SMTPPort, config.Mail, config.Password)
+	var s gomail.SendCloser
+	var err error
+	open := false
+	println("for start")
+	for {
+		println("for 1")
+		select {
+		case m, ok := <-CH:
+			println("<-CH")
+			if !ok { // 是否 close(CH)
+				return
+			}
+			if !open {
+				if s, err = d.Dial(); err != nil {
+					panic(err)
+				}
+				open = true
+			}
+			if err := gomail.Send(s, m); err != nil {
+				println(err)
+				// log.Print(err) //log?
+			}
+		case <-time.After(30 * time.Second):
+			println("<-timeout")
+			if open {
+				if err := s.Close(); err != nil {
+					panic(err)
+				}
+				open = false
+			}
+		}
+	}
 }
 
 func main() {
-	sendAlertSample("1508866205@qq.com", "test")
+	for {
+		select {
+		case <-time.After(40 * time.Second):
+			sendAlertSample("1508866205@qq.com", "test", "title")
+		}
+	}
 }
